adb8c291ad073b71ff087ad6b283024a5807bcf3
Agrippino <agrippinorussiello@outlook.it>
2022-05-23 12:35:38 +0200

    Add project files.

---
+++ webapp-travel-agency/Controllers/AreaAmministrazione.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webapp_travel_agency.Controllers
+{
+    public class AreaAmministrazione : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ webapp-travel-agency/Models/Viaggio.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapp_travel_agency.Models
+{
+    public class Viaggio
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il campo immagine per il viaggio è obbligatoria")]
+        [Url(ErrorMessage = "Non hai inserito un Url valido")]
+        public string ImmagineViaggio { get; set; }
+
+        [Required(ErrorMessage ="Il campo titolo è obbligatorio")]
+        [StringLength(25, ErrorMessage = "Hai superato il limite di caratteri massimo di 25")]
+        public string TitoloViaggio { get; set; }
+
+        [Required(ErrorMessage = "Il campo descrizione è obbligatorio")]
+        [StringLength(1000, ErrorMessage = "Hai superato il limite di caratteri massimo di 1000")]
+        [Column(TypeName = "Text")]
+        public string DescrizioneViaggio { get; set; }
+
+        [Required(ErrorMessage = "Il campo durata del viaggio è obbligatorio")]
+        public int DurataViaggio { get; set; }
+
+        [Required(ErrorMessage = "Il campo destinazioni è obbligatorio")]
+        //numero di mete da poter visitare
+        public int DestinazioniViaggio { get; set; }
+
+        [Required(ErrorMessage = "Il campo prezzo è obbligatorio")]
+        public double CostoViaggio { get; set; }
+
+
+        public Viaggio()
+        {
+
+        }
+
+        public Viaggio(string immagineViaggio,string titoloViaggio, string descrizioneViaggio, int durataViaggio, int destinazioneViaggio, double costoViaggio)
+        {
+            this.ImmagineViaggio = immagineViaggio;
+            this.TitoloViaggio = titoloViaggio;
+            this.DescrizioneViaggio = descrizioneViaggio;
+            this.DurataViaggio = durataViaggio;
+            this.DestinazioniViaggio = destinazioneViaggio;
+            this.CostoViaggio = costoViaggio;
+        }
+    }
+}
