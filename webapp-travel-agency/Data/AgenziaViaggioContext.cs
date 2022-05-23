8fdf980b09119dd9421f8dba60d928d13d78afdc
Agrippino <agrippinorussiello@outlook.it>
2022-05-23 13:10:58 +0200

    Creata Cartella Data con all'interno il database della agenzia di viaggio.

---
+++ webapp-travel-agency/Data/AgenziaViaggioContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapp_travel_agency.Models;
+
+namespace webapp_travel_agency.Data
+{
+    public class AgenziaViaggioContext : DbContext
+    {
+        public DbSet<Viaggio> Viaggi { get; set;  }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=DatabaseAgenziaDiViaggi;Integrated Security=True");
+        }
+    }
+}
