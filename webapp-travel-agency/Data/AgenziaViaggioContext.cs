6e48fb2acede8fd3a205fb564e46367b92d8d60b
Agrippino <agrippinorussiello@outlook.it>
2022-05-23 17:37:50 +0200

    Caricato database

--- webapp-travel-agency/Data/AgenziaViaggioContext.cs
+++ webapp-travel-agency/Data/AgenziaViaggioContext.cs
@@ -5,7 +5,7 @@ namespace webapp_travel_agency.Data
 {
     public class AgenziaViaggioContext : DbContext
     {
-        public DbSet<Viaggio> Viaggi { get; set;  }
+        public DbSet<Viaggio>? Viaggi { get; set;  }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=localhost;Database=DatabaseAgenziaDiViaggi;Integrated Security=True");
