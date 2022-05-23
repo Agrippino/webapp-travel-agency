8438b250bf1ce5a736b3edea4094e67ca95a6e01
Agrippino <agrippinorussiello@outlook.it>
2022-05-23 14:59:34 +0200

    Creato metodo gttp Get per cercare i viaggi disponibili

--- webapp-travel-agency/Controllers/AreaAmministrazione.cs
+++ webapp-travel-agency/Controllers/AreaAmministrazione.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using webapp_travel_agency.Data;
 using webapp_travel_agency.Models;
 
@@ -6,6 +7,7 @@ namespace webapp_travel_agency.Controllers
 {
     public class AreaAmministrazione : Controller
     {
+        //metodo get-Index che restituirà una pagina con tutti i viaggi 
         [HttpGet]
         public IActionResult Index()
         {
@@ -18,6 +20,30 @@ namespace webapp_travel_agency.Controllers
             return View("HomepageAmministrazione", ViaggiADisposizione);
         }
 
-        
+        //metodo get Dettagli per vede i dettagli di un viaggio
+        [HttpGet]
+        public IActionResult Dettagli(int id)
+        {
+            using (AgenziaViaggioContext DatabaseAgenziaDiViaggi = new AgenziaViaggioContext())
+            {
+                try
+                {
+                    // metodo per cercare i viaggi siponibili 
+                    Viaggio TrovaViaggio = DatabaseAgenziaDiViaggi.Viaggi.Where(Viaggio => Viaggio.Id == id)
+                            .Include(Viaggio => Viaggio.Id)
+                            .FirstOrDefault();
+
+                    return View("DettagliViaggio", TrovaViaggio);
+                }
+                catch (InvalidOperationException)
+                {
+                    return NotFound("Il post con id" + id + " non è stato trovato");
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+            }
+        }
     }
 }
